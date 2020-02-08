daee394317796de0021a002b09a0b92996f47347
Aaron Williams <aaronrw4@yahoo.com>
2020-02-07 20:35:53 -0800

    created get/post resource

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -8,7 +8,17 @@ function createProject(project) {
     return db('projects').insert(project)
 };
 
+function findResources() {
+    return db('resources')
+};
+
+function createResource(resource) {
+    return db('resources').insert(resource)
+}
+
 module.exports = {
     findProjects,
-    createProject
+    createProject,
+    findResources,
+    createResource
 }
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -11,15 +11,15 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                error: 'Failed to get recipes.'
+                error: 'Failed to get projects.'
             })
         })
 });
 
 router.post('/', (req, res) => {
-    const project = req.body;
+    const projectData = req.body;
 
-    projects.createProject(project)
+    projects.createProject(projectData)
         .then(project => {
             res.status(201).json(project)
         })
@@ -27,6 +27,33 @@ router.post('/', (req, res) => {
             res.status(500).json({
                 error: 'Failed to create new project'
             })
+        })
+})
+
+router.get('/resources', (req, res) => {
+    projects.findResources()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'Failed to get resources.'
+            })
+        })
+});
+
+router.post('/resources', (req, res) => {
+    const resourceData = req.body;
+
+    projects.createResource(resourceData)
+        .then(resource => {
+            res.status(201).json(resource)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'Failed to create new resource'
+            });
+            console.log(err)
         })
 })
 
